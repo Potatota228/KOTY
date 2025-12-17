e971ffa4edb54098d3d6e0bbf81fcbc21280d9a9
Potatota228 <polinadzy@gmail.com>
2025-12-17 10:50:15 +0300

    update 9 files

--- Scenes/sceneMenu.py
+++ Scenes/sceneMenu.py
@@ -1,10 +1,23 @@
 import pygame as pg
-from Scenes.scene import Scene
+from .scene import Scene
 from config import BLUE
 class MenuScene(Scene):
     def __init__(self, director):
         super().__init__(director)
-        
+        try:
+            bg_path = "./images/menu.png"
+            self.bg = pg.image.load(bg_path).convert_alpha()
+            self.bg_rect = self.bg.get_rect()
+        except Exception as e:
+            print("Не удалось загрузить фоновое изображение:", e)
+            self.bg_image = None
+        # try:
+        #     bg_path = "./images/buttons/button.png"
+        #     self.bg = pg.image.load(bg_path).convert_alpha()
+        #     self.bg_rect = self.bg.get_rect() 
+        # except Exception as e:
+        #     print("Не удалось загрузить кновки:", e)
+        #     self.bg_image = None
     def handle_events(self, events):
         for event in events:
             if event.type == pg.KEYDOWN:
@@ -13,4 +26,5 @@ class MenuScene(Scene):
     
     def render(self, screen):
         screen.fill(BLUE)
+        screen.blit(self.bg, self.bg_rect)
 
